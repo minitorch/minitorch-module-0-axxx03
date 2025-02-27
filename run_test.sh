2ed30a0b48cd7994dfba1474fab193dd08f9ddd0
scutizhang <scutizhang@tencent.com>
2025-02-27 21:10:19 +0800

    finish task0

---
+++ run_test.sh
@@ -0,0 +1,7 @@
+# streamlit run project/app.py -- 0
+
+pytest -m task0_1 > log/task0_1.log 2>&1
+pytest -m task0_2 > log/task0_2.log 2>&1
+pytest -m task0_3 > log/task0_3.log 2>&1
+pytest -m task0_4 > log/task0_4.log 2>&1
+pytest -m task0_5 > log/task0_5.log 2>&1
